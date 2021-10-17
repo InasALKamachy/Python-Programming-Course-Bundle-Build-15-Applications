6ab5f7dd79a2a45b53aad87b8826e8422c8d67c9
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-10-17 23:22:56 +0300

    Create class.py

---
+++ class.py
@@ -0,0 +1,9 @@
+class Coder():
+  def __init__(self,name,address,color):
+    self.Name = name
+    self.Address = address
+    self.Color = color
+x = Coder('enas','Iraq','red')
+print(x.Name)
+print(x.Color)
+print(x.Address

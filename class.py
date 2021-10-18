e682d7a2c07df56e04c631c01c9276b18fbcc5ca
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-10-18 23:14:22 +0300

    Update class.py

--- class.py
+++ class.py
@@ -6,4 +6,28 @@ class Coder():
 x = Coder('enas','Iraq','red')
 print(x.Name)
 print(x.Color)
-print(x.Address
+print(x.Address)
+
+
+*************************
+class Colder():
+  def __init__(self,name, age):
+    self.Name = name
+    self.Age = age
+  def Employ(self,address,color):
+    self.Address = address
+    self.Color = color
+  def is_python(self):
+    if 'Python' in self.language:
+      print("True")
+    else:
+      print("False")
+
+
+cd = Colder('enas','45')
+cd.language=['Python','C++','JavaScript']
+cd.Employ('Iraq','red')
+print(cd.Address)
+print(cd.Age)
+print(cd.Name)
+print(cd.is_python())

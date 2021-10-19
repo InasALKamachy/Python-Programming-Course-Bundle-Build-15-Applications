de2cc441823ee7baf6c78209d292543bfcc5bdd0
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-10-19 09:18:10 +0300

    Update class.py

--- class.py
+++ class.py
@@ -33,6 +33,28 @@ print(cd.Name)
 print(cd.is_python())
 
 
+**********************
+class Item():
+  def __init__(self,name,price=0,total=0):
+    assert price >=0, f"price {price} is not equal or more than zero!"
+    assert total >=0, f"total {total} is not equal or more than zero!"
+    self.name = name
+    self.price = price
+    self.total = total
+
+  def calculate_total_price(self):
+    return self.price*self.total
+  def compare(self):
+    if self.name =='Laptop':
+      print(self.name.upper())
+
+
+item = Item('Laptop', -250, -3)
+item_1 = Item('LG', 500, -5)
+print(item.calculate_total_price())
+print('the price is :',item_1.calculate_total_price())
+print(item.compare())
+
 
 ******************
 

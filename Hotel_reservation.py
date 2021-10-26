199864c80f3fe16f70417fa4e413ab49331a987f
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-10-26 10:37:28 +0300

    Update Hotel_reservation.py

--- Hotel_reservation.py
+++ Hotel_reservation.py
@@ -1,32 +1,40 @@
-class Guest:
-    regs = {'John':'A011',
-            'Kyle':'A009',
-            'Jake': 'BQ02',
-            'Tamra': 'A015',
-            'Josh': 'BQ13'}
-
+# Attempt this with paitience and remeber that you'll only succed when you fail
+# Start off by creating the Guest class and it's methods
+class Guest():
+    DT = {'John': 'A011',
+          'Kyle': 'A009',
+          'Jake': 'BQ02',
+          'Tamara': 'AO15',
+          'Josh': 'BQ13'}
     kys = ['A010', 'A012', 'A014', 'BQ01']
 
-    def __init__(self, gname):
-        self.name = (gname.lower()).capitalize()
-        self.regd = self.name in self.regs
+    def __init__(self, name):
+        self.name = (name.lower()).capitalize()
+        self.regd = self.name in self.DT
 
     def is_regd(self):
         if self.regd:
-            print("Registered")
+            print('Registered')
         else:
-            print("Not Registered")
+            print('Not Registered')
+
     def get_key(self):
         if self.regd:
-            print(f"Key : {self.regs[self.name]}")
+            print(f"key : {self.DT[self.name]}")
+        elif len(self.DT):
+            pass
+        else:
+            print('Not Registered')
+
     def reg(self):
-        if len(self.keys):
-            self.regs[self.name] = self.key[0]
+        if len(self.kys):
+            self.DT[self.name] = self.kys[0]
             self.kys.pop(0)
             self.regd = True
         else:
-            print("Sorry, no vacant rooms available")
-            
+            print('Sorry, no vacant rooms available')
+
+
 for guest_name in ['Josh', 'Hans', 'Evan', 'Kyle', 'Ted', 'Karl', 'Sam']:
     guest = Guest(guest_name)
     print(guest.name)
@@ -36,6 +44,6 @@ for guest_name in ['Josh', 'Hans', 'Evan', 'Kyle', 'Ted', 'Karl', 'Sam']:
     else:
         guest.reg()
         guest.get_key()
-        
 
-
+# very similar to what you posted except for my elif statement
+# I noticed that after the sorry no vacant rooms available under sam it still printed 'not registered' ant thoughts?

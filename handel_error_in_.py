e9d8045b230d66236ce86a53a44036a4416ca5b0
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-10-29 22:46:01 +0300

    Update handel_error_in_.py

--- handel_error_in_.py
+++ handel_error_in_.py
@@ -3,5 +3,10 @@ try:
     x = int(input("number_"))
     y = int(input("number1_"))
     print(x/y)
-except:ZeroDivisionError
+except:ZeroDivisionError:
 print("not zero ")
+except:ValueError:
+print("just type INterger value")
+except:
+   print("something error")
+

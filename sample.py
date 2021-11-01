e3e18b1d821d634f1c11469f922e3b32c09a3874
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-01 09:58:21 +0300

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,13 @@
+from random import *
+
+def cho(l):
+    '''This fucntion is used to choice N values form the list
+    coding: sample(l)'''
+    print(sample(l,2))
+
+print(help(cho))
+L = [1,2,3,4]
+N = ['a','b','c']
+
+cho(L)
+cho(N)

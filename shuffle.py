e053009419dcbf52522ce9f2399c1161f5f54f3a
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-01 09:47:44 +0300

    Update shuffle.py

--- shuffle.py
+++ shuffle.py
@@ -1,9 +1,10 @@
+from func import func
 from random import *
-
 def shu(l):
-  ''' This function is used to shuffle the value
-  coding: shuffle(l)'''
+    '''This function is used to shuffle the value
+    Coding: shuffle(l)'''
     shuffle(l)
     print(l)
 N = [1,2,3,4]
 shu(N)
+print(help(shu))

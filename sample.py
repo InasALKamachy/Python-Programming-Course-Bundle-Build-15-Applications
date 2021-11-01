1213e5cd827ea23edf41ce99d299198507bc1941
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-01 10:05:03 +0300

    Update sample.py

--- sample.py
+++ sample.py
@@ -8,6 +8,6 @@ def cho(l):
 print(help(cho))
 L = [1,2,3,4]
 N = ['a','b','c']
-
+seed = 7 # '''The seed() method is used to initialize the random number generator.The random number generator needs a number to start with (a seed value), to be able to generate a random number. '''
 cho(L)
 cho(N)

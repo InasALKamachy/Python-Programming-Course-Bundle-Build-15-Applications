85fc6f26c46f71e78443b6b7c9927d8158ecbee2
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-01 09:54:25 +0300

    Create choice.py

---
+++ choice.py
@@ -0,0 +1,13 @@
+from random import *
+
+def cho(l):
+    '''This fucntion is used to choice a value form the list
+    coding: choice(l)'''
+    print(choice(l))
+
+print(help(cho))
+L = [1,2,3,4]
+N = ['a','b','c']
+
+cho(L)
+cho(N)

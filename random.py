a8b35fd42654391d8dbd7096520b9a7452a65a78
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-01 09:26:42 +0300

    Create random.py

---
+++ random.py
@@ -0,0 +1,8 @@
+from random import *
+
+print(random()) # between 0.0 to 1.0
+print(randint(-1,5)) # between two integer number positive or negative
+print(uniform(0.1,0.9)) # between two float number
+print(round(random(),2))
+print(randrange(0,100,2)) # give begin, end, step , here print even number
+print(randrange(1,101,2)) # give odd number

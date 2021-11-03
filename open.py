3643f58eb55ba4b24b6160ef9bfa771e8180bb5e
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-03 22:46:16 +0300

    Update open.py

--- open.py
+++ open.py
@@ -3,3 +3,9 @@ print(y.write('heljkajdf')
 y = open('text.txt', mode = 'r')
 
 print(y.read)
+      
+      
+      
+      
+#open mode parameter values
+      https://img-c.udemycdn.com/redactor/raw/article_lecture/2021-01-09_08-18-15-16edbdf446a8c971d8168ad1365736eb.jpg

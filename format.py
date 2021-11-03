14b1e1449b12dbd4a28a0ecbe06840904718cf7b
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-03 10:37:13 +0300

    Update format.py

--- format.py
+++ format.py
@@ -1,3 +1,20 @@
+def grting(x):
+    x = input("Enter your name")
+    print("Hello"+x)
+
+def li(l):
+    x = enumerate(l)
+    print(list(x))
+
+def even(num):
+    if num % 2 ==0:
+        return True
+    else:
+        return False
+def exact(num):
+    '''return the exact value of number '''
+    return format(num,'d')
+
 def exact(num):
     '''return the exact value of number '''
     return format(num,'d')
@@ -34,6 +51,42 @@ def roun(num):
     '''return the round number to 6 default use g or G  '''
     return format(num,'g')
 
+def forc(num):
+    '''force to assign to left < or right> or center ^ or most left = '''
+    return format(num,'>')
+def bina(num):
+    '''return the binary value of number '''
+    return format(num,'b')
+
+def uni(num):
+    '''return the unicode character value of number '''
+    return format(num,'c')
+
+def oct(num):
+    '''return the octal value of number '''
+    return format(num,'o')
+
+def hex(num):
+    '''return the hexal value of number '''
+    return format(num,'x')
+
+def deci(num):
+    '''return the exact value of number '''
+    return format(num,'d')
+
+def exp(num):
+    '''return the exponential value of number '''
+    return format(num,'e')
+def fix(num):
+    '''return the fixpoint of value where 6 is default'''
+    return format(num,'f')
+
+def roun(num):
+    '''return the round number to 6 default use g or G  '''
+    return format(num,'g')
+
 def percen(num):
     '''multiply the number by 100 and put % at the end '''
     return format(num,'%')
+
+

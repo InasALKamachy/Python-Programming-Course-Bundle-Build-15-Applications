6b6a5b0522b665a62ba6eccd985228556b529b50
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-03 08:23:00 +0300

    Update enumerate.py

--- enumerate.py
+++ enumerate.py
@@ -3,5 +3,5 @@ L = ['E','b','c','d']
 x = enumerate(L)
 print(list(x))
 # to print as index and value
-for ind,lan in enumerate(L):
+for ind,lan in enumerate(L, start=1): # make the index begin with i as value
     print(ind, ':', lan)

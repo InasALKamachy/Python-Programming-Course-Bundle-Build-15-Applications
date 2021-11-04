b1b485a03adfb85870a1e4bc6160661f9a7a81dc
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-04 11:55:00 +0300

    Update solution.py

--- WorkBook+(Intermediate)/solution.py
+++ WorkBook+(Intermediate)/solution.py
@@ -9,5 +9,56 @@ else:
   print(lst)
   
   
+##Q2 
+degree_ = [65,80,33]
+student_ = ['S1_marks', 'S2_marks', 'S3_marks']
+
+
+for i, j in list(zip(degree_, student_)):
+  print(j + '=' +str(i))
+for i in degree_:
+  if i >=80:
+    print('Ist division')
+  elif i>=60:
+    print('IIst division')
+  elif i>=33:
+    print('III division')
+  else:
+    print('Failed')
+
+ ## Q3
+c= True
+d = False
+
+a = 5
+b = 5.0
+if a==b:
+  print(c)
+else:
+  print(d)
+  
+ ##Q4
+nothing wrong
+
+##Q5
+$$ and if tru && tur ===> true, True && False ==> False
+|| or  if True || True ==> True, True || False ==> True
+
+##Q1
+While Looop
+l = [11,12,13,14,15]
+
+i=True
+while i ==True:
+  for j in l:
+    print(pow(j,2))
  
+  break
+
+
+##Q2
+
+  
+##Q7
+
 

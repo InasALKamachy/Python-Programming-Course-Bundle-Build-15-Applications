7b13491f6d74568674a6044fa1e6821c07039be5
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-05 23:40:45 +0300

    Update grid.py

--- GUI/grid.py
+++ GUI/grid.py
@@ -5,33 +5,34 @@ App = Tk()
 App.title('Dice_App') # title in taskbar
 App.geometry('400x100')#size of window
 Inp = Entry(App) ##input
-Inp.grid(row = 0, column = 0, columnspan = 2)
+Inp.grid(row = 0, column = 0, columnspan = 2, padx = 25, pady = 5)
 def random_():
     Display = Label(App, text=randint(1,6))  # title in main window
-    Display.grid(row = 3, column = 0)
+    Display.grid(row = 3, column = 0, padx = 25, pady = 5)
 def show():
     inp = Inp.get()
     msg = Label(App, text=inp)
-    msg.grid(row = 3, column = 1)
+    msg.grid(row = 3, column = 1, padx = 25, pady = 5)
 
 Inp1 = Entry(App) ##input
-Inp1.grid(row = 0, column = 2, columnspan = 2)
+Inp1.grid(row = 0, column = 2, columnspan = 2, padx = 25, pady = 5)
 
 def chose():
     inp = (Inp1.get()).split(',')
-    msg = Label(App, text=choice(inp))
-    msg.grid(row = 3, column = 2)
+    choic = ('The Choice is :'+str(choice(inp)))
+    msg = Label(App, text=choic)
+    msg.grid(row = 0, column = 5, padx = 25, pady = 5)
 
 
 B = Button(App, text = 'Click', command=random_)
-B.grid(row = 1, column = 0)
+B.grid(row = 1, column = 0, padx = 25, pady = 5)
 
 A = Button(App, text = 'Show', command=show)
-A.grid(row = 1, column = 1)
+A.grid(row = 1, column = 1, padx = 25, pady = 5)
 
 A = Button(App, text = 'Choose', command=chose)
-A.grid(row = 1, column = 2)
+A.grid(row = 1, column = 2, padx = 25, pady = 5)
 
 quit = Button(App, text = 'Cancel', command=App.quit)
-quit.grid(row = 1, column = 3)
+quit.grid(row = 1, column = 3, padx = 25, pady = 5)
 App.mainloop()# must be at the end

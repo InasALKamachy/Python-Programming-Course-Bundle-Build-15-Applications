66b609649e2a755ff6e323c35c059c063d2470c2
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-07 12:36:30 +0300

    Update check_box.py

--- GUI/check_box.py
+++ GUI/check_box.py
@@ -14,3 +14,27 @@ def show():
 B = Button(App, text = 'show', command = show)
 B.pack()
 App.mainloop()
+
+
+
+##########################################
+
+from tkinter import *
+
+App = Tk()
+App.geometry('200x100')
+App.title("check_box")
+App['background'] = 'pink'
+check = StringVar()
+
+chk = Checkbutton(App, text = 'check_here',  onvalue = 'Yes',offvalue = 'No', variable = check)
+chk.deselect()
+chk.pack()
+
+
+def show():
+    msg = Label(App,text=check.get())
+    msg.pack()
+B = Button(App, text = 'show', command = show)
+B.pack()
+App.mainloop()

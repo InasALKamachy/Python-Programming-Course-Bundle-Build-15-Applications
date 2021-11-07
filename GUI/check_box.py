c61cad027d458770cd2693e559c0a8d10735c98f
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-07 21:41:24 +0300

    Update check_box.py

--- GUI/check_box.py
+++ GUI/check_box.py
@@ -38,3 +38,43 @@ def show():
 B = Button(App, text = 'show', command = show)
 B.pack()
 App.mainloop()
+
+#############################################
+###built check box to choose two value :
+
+import random
+from tkinter import *
+from random import *
+App = Tk()
+App['background'] = '#2A3638'
+App.title('Dice_App') # title in taskbar
+App.geometry('250x100')#size of window
+Inp1 = Entry(App, background = '#E84A5F', foreground = '#FECEAB') ##input
+Inp1.grid(row = 0, column = 0, columnspan = 2, padx = 25, pady = 5)
+
+check =IntVar()
+chk = Checkbutton(App, text = 'Double',  onvalue = 2,offvalue = 1, variable = check)
+chk.deselect()
+chk.grid(row = 1, column = 0, columnspan = 2, padx = 25, pady = 5)
+
+def chose():
+    inp = (Inp1.get()).split(',')
+    if check.get() == 2:
+        choic = ('The Choice is :'+str(sample(inp, 2)))
+    else:
+        choic = ('The Choice is :' + str(sample(inp, 1)))
+    msg = Label(App, text=choic, font = ('Courier',10), background = '#E84A5F', foreground = '#FECEAB', relief = 'raise', width = 30, borderwidth = 4)# or ridge or raise or sunken or flat or groove
+
+    msg.grid(row = 2, column = 0, padx = 25, pady = 5, columnspan = 2)
+    if quit['stat'] == DISABLED:
+        quit['stat']=NORMAL
+
+
+
+
+A = Button(App, text = 'Choose', command=chose, background = '#FF847C', foreground = '#FECEAB',relief = 'groove', borderwidth = 4)
+A.grid(row = 3, column = 0, padx = 25, pady = 5)
+
+quit = Button(App, text = 'Cancel', command=App.quit, stat = DISABLED, background = 'red', foreground = '#FECEAB', borderwidth = 4)
+quit.grid(row = 3, column = 1, padx = 25, pady = 5)
+App.mainloop()# must be at the end

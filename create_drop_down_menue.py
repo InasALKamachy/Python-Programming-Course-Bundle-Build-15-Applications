b63b3880c193d9ff366a08d282efa228665a6191
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-09 12:06:47 +0300

    Update create_drop_down_menue.py

--- create_drop_down_menue.py
+++ create_drop_down_menue.py
@@ -4,6 +4,7 @@ App.geometry('300x200')
 option_menue= StringVar()
 lst = ['red', 'white', 'green']
 sld = OptionMenu(App, option_menue, *lst)
+option_menue.set('white')
 sld.pack()
 def show():
     msg = Label(App, text = ' ', background = (option_menue.get()).lower())

9929f781d49a5e7e8c74acdc4d7196b65c41192f
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-09 12:04:21 +0300

    Create create_drop_down_menue.py

---
+++ create_drop_down_menue.py
@@ -0,0 +1,15 @@
+from tkinter import *
+App = Tk()
+App.geometry('300x200')
+option_menue= StringVar()
+lst = ['red', 'white', 'green']
+sld = OptionMenu(App, option_menue, *lst)
+sld.pack()
+def show():
+    msg = Label(App, text = ' ', background = (option_menue.get()).lower())
+    msg.pack()
+
+B = Button(App, text = 'show', command = show)
+B.pack()
+
+App.mainloop()

f5f364841cb206f4f0feb851843ee2635052f141
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-11-11 10:07:49 +0300

    Create replit.py

---
+++ GUI/image/replit.py
@@ -0,0 +1,11 @@
+
+from tkinter import *
+from PIL import Image ,ImageTk
+
+
+app = Tk()
+app.title('App')
+app['background'] = '#2A3638'
+app.iconphoto(True, PhotoImage(file="res/python1.png"))
+
+app.mainloop()
